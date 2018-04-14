8613d9d6f7738900192445fc67569387be1a1a01
felipeigua5 <felipeigua5@gmail.com>
2018-04-14 13:16:07 -0500

    textview

--- app/src/main/java/com/example/aula7/selectorcolor/MainActivity.java
+++ app/src/main/java/com/example/aula7/selectorcolor/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     int seekR, seekG, seekB, seekA;
     SeekBar seekBarRed, seekBarGreen, seekBarBlue, seekBarAlpha;
     View viewBackgraound;
+     TextView tvGreen;
+     TextView tvRed;
+     TextView tvBlue;
+     TextView tvAlpha;
 
 
     @Override
@@ -24,16 +29,29 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         seekBarAlpha = (SeekBar) findViewById(R.id.id_sb_alpha);
         viewBackgraound = (View) findViewById(R.id.id_v_background);
 
+        tvRed = (TextView) findViewById(R.id.tvRed);
+        tvBlue = (TextView) findViewById(R.id.tvBlue);
+        tvGreen = (TextView) findViewById(R.id.tvGreen);
+        tvAlpha = (TextView) findViewById(R.id.tvAlpha);
+
         seekBarAlpha.setOnSeekBarChangeListener(this);
         seekBarBlue.setOnSeekBarChangeListener(this);
         seekBarRed.setOnSeekBarChangeListener(this);
         seekBarGreen.setOnSeekBarChangeListener(this);
+
+        updateBackground();
+
     }
     public void updateBackground(){
         seekR = seekBarRed.getProgress();
         seekG = seekBarGreen.getProgress();
         seekB = seekBarBlue.getProgress();
         seekA = seekBarAlpha.getProgress();
+
+        tvRed.setText(String.valueOf(seekR));
+        tvBlue.setText(String.valueOf(seekB));
+        tvGreen.setText(String.valueOf(seekG));
+        tvAlpha.setText(String.valueOf(seekA));
 
         int color = Color.argb(seekA, seekB, seekG, seekR);
         viewBackgraound.setBackgroundColor(color);
